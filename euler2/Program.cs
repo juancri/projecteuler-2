930ec4b4de156f7741a6fb2a74557a7f08e877f7
Juan C. Olivares <cristobal@cxsoftware.com>
2014-10-10 15:08:05 -0300

    First commit ;)

---
+++ euler2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MainClass
+{
+	public static void Main ()
+	{
+		var res = Fib ()
+			.TakeWhile (x => x <= 4000000)
+			.Where (x => (x & 1) == 0)
+			.Sum ();
+		Console.WriteLine (res);
+	}
+
+	private static IEnumerable <int> Fib ()
+	{
+		var old1 = 0;
+		var old2 = 1;
+		while (true)
+		{
+			var current = old1 + old2;
+			old1 = old2;
+			yield return (old2 = current);
+		}
+	}
+}
